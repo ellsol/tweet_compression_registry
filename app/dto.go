57c9eacb67ff0d5b0e9a00d40002b219d61088a0
MikaBuchholz <mika.buchholz3@gmail.com>
2023-03-27 18:59:51 +0200

    Added GET tweet dto and validation

--- app/dto.go
+++ app/dto.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 // swagger:model UploadTweetDTO
@@ -11,10 +13,19 @@ type UploadTweetDTO struct {
 	Payload string `json:"payload"`
 }
 
+type GetTweetDTO struct {
+	Checksum string
+}
+
 // swagger:model UploadTweetResponseDTO
 type UploadTweetResponseDTO struct {
 	Id       string `json:"id"`
 	Checksum string `json:"checksum"`
+}
+
+type GetTweetResponseDTO struct {
+	Id            string `json:"id"`
+	Tweet_Content string `json:"tweet"`
 }
 
 func (a *UploadTweetDTO) ReadAndValidate(r *http.Request) error {
@@ -27,9 +38,28 @@ func (a *UploadTweetDTO) ReadAndValidate(r *http.Request) error {
 	return a.Validate()
 }
 
+func (a *GetTweetDTO) ReadAndValidate(r *http.Request) error {
+	checksum_val := chi.URLParam(r, "checksum")
+
+	if checksum_val == "" {
+		return fmt.Errorf("body is empty")
+	}
+
+	a.Checksum = checksum_val
+
+	return a.Validate()
+}
+
 func (data *UploadTweetDTO) Validate() error {
 	if data.Payload == "" {
 		return fmt.Errorf("payload cannot be empty")
+	}
+	return nil
+}
+
+func (data *GetTweetDTO) Validate() error {
+	if data.Checksum == "" {
+		return fmt.Errorf("checksum cannot be empty")
 	}
 	return nil
 }
