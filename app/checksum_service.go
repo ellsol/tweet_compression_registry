d24a3a80250771caed0a5e300be83b52cab1e8e2
MikaBuchholz <mika.buchholz3@gmail.com>
2023-03-28 20:12:57 +0200

    Implemented pagination to get tweets

--- app/checksum_service.go
+++ app/checksum_service.go
@@ -41,6 +41,32 @@ func (s *ChecksumService) GetTweet(context context.Context, data *GetTweetDTO) (
 	}, nil
 }
 
+func (s *ChecksumService) GetPaginatedTweets(context context.Context, data *PaginationDTO) (*PaginatedTweetsResponseDTO, error) {
+	stmt := Tweet.SELECT(Tweet.Original, Tweet.ID).FROM(Tweet)
+
+	stmt = stmt.OFFSET(int64(data.offset)).LIMIT(int64(data.limit))
+
+	dest := []model.Tweet{}
+	err := stmt.Query(s.db, &dest)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var tweets []GetTweetResponseDTO
+
+	for _, tweet_model := range dest {
+		fmt.Println(tweet_model)
+		tweet := GetTweetResponseDTO{Id: tweet_model.ID.String(), Tweet_Content: tweet_model.Original}
+		tweets = append(tweets, tweet)
+	}
+
+	return &PaginatedTweetsResponseDTO{
+		Tweets: tweets,
+	}, nil
+
+}
+
 func (s *ChecksumService) NewTweet(context context.Context, data *UploadTweetDTO) (*UploadTweetResponseDTO, error) {
 	c := sha256.Sum256([]byte(data.Payload))
 	check := hex.EncodeToString(c[:])
